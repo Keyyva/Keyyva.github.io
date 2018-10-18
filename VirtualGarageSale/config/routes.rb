0cebe411d603d97d1ab8b1e3e9504edcad3e6b19
Keyyva <keyyva@gmail.com>
2018-10-18 00:59:00 -0400

    Created own welcome page. Updated ReadMe.md

--- VirtualGarageSale/config/routes.rb
+++ VirtualGarageSale/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   	registrations: 'registrations'
   }
   
-  #root 'store#index'
-  root 'products#index'
+  root 'store#index'
+  #root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
